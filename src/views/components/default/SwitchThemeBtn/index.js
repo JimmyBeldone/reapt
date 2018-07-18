cc18851d25a77f92051571a15e5f7238f859012c
JimmyBeldone <dev.jimmy.beldone@gmail.com>
2018-07-18 11:20:12 +0200

    App - Added SwitchThemeBtn

---
+++ src/views/components/default/SwitchThemeBtn/index.js
@@ -0,0 +1,25 @@
+import React, {Fragment} from 'react'
+import PropTypes from 'prop-types'
+
+import {ThemeConsumer} from '../../../../providers/ThemeProvider'
+
+const SwitchThemeBtn = () => (
+    <div className="switch-theme">
+        Theme:
+        <ThemeConsumer>
+            {({toggleTheme, theme}) => (
+                <Fragment>
+                    <input
+                        id="theme"
+                        type="checkbox"
+                        onChange={toggleTheme}
+                        checked={theme === 'dark'}
+                    />
+                    <label htmlFor="theme">Dark</label>
+                </Fragment>
+            )}
+        </ThemeConsumer>
+    </div>
+)
+
+export default SwitchThemeBtn
--- src/views/containers/Header/Header.js
+++ src/views/containers/Header/Header.js
@@ -1,31 +1,15 @@
-import React, {PureComponent, Fragment} from 'react'
+import React, {PureComponent} from 'react'
 import PropTypes from 'prop-types'
 
 import './Header.styl'
-import {ThemeConsumer} from '../../../providers/ThemeProvider'
 import SwitchLangBtn from '../../components/default/SwitchLangBtn'
+import SwitchThemeBtn from '../../components/default/SwitchThemeBtn'
 
 class Header extends PureComponent {
     render () {
         return (
             <header>
-                <h1>Header</h1>
-                <div className="switch-theme">
-                    Theme:
-                    <ThemeConsumer>
-                        {({toggleTheme, theme}) => (
-                            <Fragment>
-                                <input
-                                    id="theme"
-                                    type="checkbox"
-                                    onChange={toggleTheme}
-                                    checked={theme === 'dark'}
-                                />
-                                <label htmlFor="theme">Dark</label>
-                            </Fragment>
-                        )}
-                    </ThemeConsumer>
-                </div>
+                <SwitchThemeBtn />
                 <SwitchLangBtn />
             </header>
         );
