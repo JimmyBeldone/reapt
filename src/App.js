708e5a21006b78e345a003337e94a71e99338d9c
JimmyBeldone <dev.jimmy.beldone@gmail.com>
2018-07-23 12:01:14 +0200

    App - Check

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Router, Switch, Redirect} from 'react-router-dom'
+import noInternet from 'no-internet'
+import isEmpty from 'lodash/isEmpty'
 
 import RouteWithSubRoutes from './routes/RouteWithSubRoutes'
 import getRoutesByEnv from './routes'
@@ -19,6 +21,25 @@ class App extends Component {
         history: PropTypes.object
     }
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOffline: false
+        }
+    }
+
+    componentDidMount() {
+        noInternet({
+            callback: offline => {
+                if (offline) {
+                    this.setState({isOffline: true})
+                } else {
+                    this.setState({isOffline: false})
+                }
+            }
+        })
+    }
+
     render () {
         const {history} = this.props
         const style = {color: 'red'}
@@ -32,6 +53,11 @@ class App extends Component {
                         </Switch>
                     </Router>
                 </DefaultLayout>
+                {this.state.isOffline ? (
+                    <div className="offline-info">
+                        You are offline
+                    </div>
+                ) : null}
             </ModalProvider>
         )
     }
