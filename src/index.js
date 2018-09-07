508af8fdaea4b897ca66f0a3e16ae44a7e1dd4d7
JimmyBeldone <dev.jimmy.beldone@gmail.com>
2018-09-07 15:17:04 +0200

    App - Removed loadPolyfill.js in favor of babel useBuiltIns: usage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import loadPolyfills from './loadPolyfills'
+// Loads vendors and global style
+import './styles/global.styl'
+// Loading fonts style sheet separately for webpack to load all files in dist folder
+import './assets/fonts/fonts.styl'
 
-loadPolyfills().then(import('./setup'))
+import React from 'react'
+import { render } from 'react-dom'
+
+import configureStore, { history } from './store/configureStore'
+import Root from './Root'
+import registerServiceWorker from './registerServiceWorker'
+
+const store = configureStore()
+window.lang = 'fr'
+
+render(
+    <Root store={store} history={history} />,
+    document.getElementById('app')
+)
+
+registerServiceWorker()
