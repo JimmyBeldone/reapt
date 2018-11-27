c5ef6cc28378b01d77d860783f566fcfdeeafb86
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2018-11-27 15:54:30 +0100

    fix(lang): remove duplication

--- src/lang/en/index.js
+++ src/lang/en/index.js
@@ -30,18 +30,14 @@ module.exports = {
         }
     },
     errors: {
-        emptyField: "Veillez remplir le champ {field}",
-        invalidEmail: "Le format d'email est invalide",
-        identical: "L'ancien mot de passe est identique au nouveau",
-        notIdentical: "Les mots de passe ne sont pas identiques",
-        wrongPassword: "L'ancien mot de passe est incorrect",
-        wrongCredentials: "Vos identifiants sont incorrects",
-        outdatedSubscription: "Votre abonnement est expiré",
-        tokenNotFound: "Clef d'inscription introuvable",
-        passwordNotFound: "Mot de passe non renseigné",
-        userNotFoundWithToken:
-            "Utilisateur introuvable avec cette clef d'inscription",
-        accountDisabled: "Compte désactivé",
-        noDatas: "Pas de données pour les actes selectionnés"
+        emptyField: "Field {field} is empty",
+        invalidEmail: "Invalid mail format",
+        identical: "Old password equals new password",
+        notIdentical: "Password are not equals",
+        wrongPassword: "Incorrect old password",
+        wrongCredentials: "Wrong credentials",
+        tokenNotFound: "Cannot find token",
+        passwordNotFound: "Unregistered password",
+        userNotFoundWithToken: "Cannot find any user with this token"
     }
 };
