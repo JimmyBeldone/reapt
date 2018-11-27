a4d5166e76902d387ad93e4b748fd13ec514c5a5
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2018-11-27 16:20:49 +0100

    refactor(format): merge all format components into one

--- src/views/components/default/Format/Decimal.js
+++ src/views/components/default/Format/Decimal.js
@@ -5,23 +5,41 @@ import numbro from "numbro";
 /* eslint react/style-prop-object: 0 */
 
 const Decimal = memo(
-    ({ className, isEvolution, value, fractionDigits, average }) => (
-        <span className={className}>
-            {value === null || Number.isNaN(value)
-                ? "N/A"
-                : numbro(value).format({
-                      mantissa: fractionDigits,
-                      forceSign: isEvolution && value > 0,
-                      thousandSeparated: true,
-                      spaceSeparated: false,
-                      optionalMantissa: true,
-                      average
-                  })}
-        </span>
-    )
+    ({
+        className,
+        isEvolution,
+        value,
+        fractionDigits,
+        average,
+        isCurrency,
+        isPercent
+    }) => {
+        const options = {
+            mantissa: fractionDigits,
+            forceSign: isEvolution && value > 0,
+            thousandSeparated: true,
+            spaceSeparated: false,
+            optionalMantissa: true,
+            average
+        };
+        if (isPercent) {
+            options.output = "percent";
+            delete options.average;
+        }
+        const format = isCurrency
+            ? numbro(value).formatCurrency(options)
+            : numbro(value).format(options);
+        return (
+            <span className={className}>
+                {value === null || Number.isNaN(value) ? "N/A" : format}
+            </span>
+        );
+    }
 );
 
 Decimal.propTypes = {
+    isPercent: PropTypes.bool,
+    isCurrency: PropTypes.bool,
     value: PropTypes.number.isRequired,
     className: PropTypes.string,
     isEvolution: PropTypes.bool,
@@ -30,6 +48,8 @@ Decimal.propTypes = {
 };
 
 Decimal.defaultProps = {
+    isCurrency: false,
+    isPercent: false,
     isEvolution: false,
     className: "",
     fractionDigits: 2,
