ad02f75559f94dbc24b424a4f9c2395a2aef4748
Jimmy Beldone <dev.jimmy.beldone@gmail.com>
2018-12-07 16:31:42 +0100

    test: comment InputGroup test

--- src/views/components/default/InputGroup/InputGroup.testTODO.js
+++ src/views/components/default/InputGroup/InputGroup.testTODO.js
@@ -1,32 +1,32 @@
-import React from "react";
-import { render, shallow, mount } from "enzyme";
-import InputGroup from "./InputGroup";
+// import React from "react";
+// import { render, shallow, mount } from "enzyme";
+// import InputGroup from "./InputGroup";
 
-describe("Test InputGroup Component", () => {
-    let props = {
-        name: "login",
-        label: "Login",
-        // ref: "input-login",
-        errorField: "",
-        withIntl: false
-    };
+// describe("Test InputGroup Component", () => {
+//     let props = {
+//         name: "login",
+//         label: "Login",
+//         // ref: "input-login",
+//         errorField: "",
+//         withIntl: false
+//     };
 
-    it("Should render", () => {
-        const wrapper = render(<InputGroup {...props} />);
-    });
+//     it("Should render", () => {
+//         const wrapper = render(<InputGroup {...props} />);
+//     });
 
-    it("Should works with erroField", () => {
-        const wrapper = render(<InputGroup {...props} errorField={"Login"} />);
-    });
+//     it("Should works with erroField", () => {
+//         const wrapper = render(<InputGroup {...props} errorField={"Login"} />);
+//     });
 
-    it("Should works with erroField", () => {
-        const wrapper = shallow(<InputGroup {...props} errorField={"Login"} />);
-        expect(wrapper.exists(".error")).toBe(true);
-    });
+//     it("Should works with erroField", () => {
+//         const wrapper = shallow(<InputGroup {...props} errorField={"Login"} />);
+//         expect(wrapper.exists(".error")).toBe(true);
+//     });
 
-    it("Should works with ref", () => {
-        let ref = null;
-        const wrapper = mount(<InputGroup {...props} />);
-        expect(wrapper.instance().input).toBeTruthy();
-    });
-});
+//     it("Should works with ref", () => {
+//         let ref = null;
+//         const wrapper = mount(<InputGroup {...props} />);
+//         expect(wrapper.instance().input).toBeTruthy();
+//     });
+// });
